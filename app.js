0a3c85ab41e4d0aa9f3ef2b82944d2b9325280c7
keuspastis <philippe.tome@protonmail.com>
2020-03-17 17:05:08 +1100

    final version

---
+++ app.js
@@ -0,0 +1,43 @@
+const menuButton = document.querySelector('.menuButton');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.navigationBar');
+const navItems = document.querySelectorAll('.navItem');
+
+let showMenu = false;
+menuButton.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    menuButton.classList.add('close');
+    menu.classList.add('show');
+    menuNav.classList.add('show');
+    navItems.forEach(item => item.classList.add('show'));
+    showMenu = true;
+  } else {
+    menuButton.classList.remove('close');
+    menu.classList.remove('show');
+    menuNav.classList.remove('show');
+    navItems.forEach(item => item.classList.remove('show'));
+    showMenu = false;
+  }
+}
+
+$('nav')
+  .find('.pageLink')
+  .click(function(e) {
+    menuButton.classList.remove('close');
+    menu.classList.remove('show');
+    menuNav.classList.remove('show');
+    navItems.forEach(item => item.classList.remove('show'));
+    showMenu = false;
+    let section = $(this).attr('href');
+    $('html, body').animate({
+      scrollTop: $(section).offset().top
+    });
+});
+
+$('.videoPlayer').hide();
+$('#viewDemo').click(function(e) {
+  e.preventDefault();
+  $('.videoPlayer').fadeIn();
+});
